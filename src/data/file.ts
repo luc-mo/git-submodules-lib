ace46bcae6a43ce410fb6a37ba0ebe4efc0850d1
SnowDrive <luc.moralesr@gmail.com>
2025-07-08 10:38:31 -0400

    feat: add axios lib and a request call

--- src/data/file.ts
+++ src/data/file.ts
@@ -1,3 +1,6 @@
-export const main = () => {
-  console.log('hola')
+import axios from 'axios'
+
+export const main = async() => {
+  const data = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  console.log({ data })
 }
