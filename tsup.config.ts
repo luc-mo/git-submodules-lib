1e9d728c6547d403ad95edee2ad5f191e8264c9f
SnowDrive <luc.moralesr@gmail.com>
2025-07-08 04:55:19 -0400

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['src/index.ts'],
 	tsconfig: './tsconfig.json',
+	format: ['cjs', 'esm'],
 	clean: true,
 	treeshake: false,
 	minify: false,
